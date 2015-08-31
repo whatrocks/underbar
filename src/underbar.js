c4ee5c61a5c15624be0785ee599d04a5ad7fd1a9
Charles Harrington <charles.harrington@gmail.com>
2015-08-31 11:20:25 -0400

    Completed last function

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === undefined) {
+      return array.pop();
+    } else if (n === 0) {
+      return [];
+    } else {
+      return array.slice(-n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
